c1a76da1a5eef257e252346a5b4ace3a6631809b
IlyaUsikov <IlyaUsikov@coherentsolutions.com>
2018-05-07 15:17:00 +0300

    Added delayed initialization of tortuga DOM object

--- tortuga.js
+++ tortuga.js
@@ -5,7 +5,11 @@
     Tortuga = Object.create(new Movable("tortuga"));
     Tortuga.moveUp = function(){};
     Tortuga.moveDown = function(){};
+    Tortuga.init = function() {
+        
+        console.log("Tortuga initialization...");
 
-    Tortuga.element.style.left = 10 + 'px';
-    Tortuga.element.style.top = Tortuga.getFloatParentHeight() - Tortuga.getFloatHeight() + 'px';
+        Tortuga.element.style.left = 10 + 'px';
+        Tortuga.element.style.top = Tortuga.getFloatParentHeight() - Tortuga.getFloatHeight() + 'px';
+    };
 })();
