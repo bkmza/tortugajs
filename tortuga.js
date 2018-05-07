1f38b20760b0333461bafba3f8b139a97881986a
IlyaUsikov <IlyaUsikov@coherentsolutions.com>
2018-05-07 17:52:36 +0300

    Moved ruling.js to tortuga.js

--- tortuga.js
+++ tortuga.js
@@ -13,4 +13,31 @@
         Tortuga.element.style.left = 10 + 'px';
         Tortuga.element.style.top = Tortuga.getFloatParentHeight() - Tortuga.getFloatHeight() + 'px';
     };
+
+    window.addEventListener("keydown", function(e) {
+        switch(e.keyCode) {
+            case 37:
+                if(!Tortuga.isInLeftZone()) {
+                    Tortuga.moveLeft();
+                }
+            break;
+            case 39:
+                if(Tortuga.isInLeftZone()) {
+                    Tortuga.moveRight();
+                }                
+            break;
+            case 38:
+                Tortuga.moveUp();
+            break;
+            case 40:
+                Tortuga.moveDown();
+            break;
+            default:
+                console.log("Not supported action.");
+        }
+    });
+
+    getRandomInt = function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
 })();
